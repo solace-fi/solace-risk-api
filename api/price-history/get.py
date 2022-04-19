cab6668243f00bee6b8c17c97b28c09b94d9b08b
selcuk sozuer <selcuk.sozuer@gmail.com>
2022-04-19 11:01:43 +0300

    adding try/catch

--- api/price-history/get.py
+++ api/price-history/get.py
@@ -1,7 +1,7 @@
 from api.utils import *
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 
-API_KEY = "ckey_a0dadddb07fe400a95879111873"
+
 def __get_thickers(params):
     tickers = []
     if "tickers" not in params:
@@ -19,42 +19,45 @@ def __calculate_price_change(prev_price_data: dict, curr_price_data: dict):
     return {"date": curr_price_data["date"], "price": curr_price,  "change": percentage}
 
 def get_price_history(params):
-    tickers = __get_thickers(params)
-    start_date = get_date_string()
-    end_date = get_date_string()
+    try:
+        tickers = __get_thickers(params)
+        start_date = get_date_string()
+        end_date = get_date_string()
 
-    window = 7
-    if "window" in params:
-        try:
-            window = int(params["window"])
-        except:
-            print(f"window param { params['window'] } is not valid")
-    
-    end_date = datetime.now()
-    start_date = end_date - timedelta(days=window)
-    start_date = start_date.strftime("%Y-%m-%d")
-    end_date = end_date.strftime("%Y-%m-%d")
+        window = 7
+        if "window" in params:
+            try:
+                window = int(params["window"])
+            except:
+                print(f"window param { params['window'] } is not valid")
+        
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=window)
+        start_date = start_date.strftime("%Y-%m-%d")
+        end_date = end_date.strftime("%Y-%m-%d")
 
-    result = {}
-    for ticker in tickers:
-        try:
-            url = f"https://api.covalenthq.com/v1/pricing/historical/USD/{ticker}/?quote-currency=USD&format=JSON&from={start_date}&to={end_date}&key={API_KEY}"
-            response = requests.get(url, timeout=600)
-            response.raise_for_status()
-            response = response.json()
-            response = response["data"]
-            # get price history
-            result[ticker] = list(map(lambda price: {"date": price["date"], "price": price["price"]}, response["prices"]))
+        result = {}
+        for ticker in tickers:
+           print(f"Getting price info for ticker: {ticker}")
+           url = f"https://api.covalenthq.com/v1/pricing/historical/USD/{ticker}/?quote-currency=USD&format=JSON&from={start_date}&to={end_date}&key={COVALENT_API_KEY}"
+
+           response = requests.get(url, timeout=600)
+           response.raise_for_status()
+           response = response.json()
+           response = response["data"]
            
-            # calculate price change
-            result[ticker] = [__calculate_price_change(result[ticker][i-1], result[ticker][i]) for i in range(1, len(result[ticker])-1)]
-            
-        except Exception as e:
+           # get price history
+           result[ticker] = list(map(lambda price: {"date": price["date"], "price": price["price"]}, response["prices"]))
+           
+           # calculate price change
+           result[ticker] = [__calculate_price_change(result[ticker][i-1], result[ticker][i]) for i in range(1, len(result[ticker])-1)]
+
+        return result
+    except Exception as e:
             msg = f"In Solace Risk API get price history. Error fetching price history for {ticker}\nURL   : {url}\nIP    : {get_IP_address()}\nError : {e}"
             print(msg)
             sns_publish(msg)
-    return result
-        
+    return {}
 
 def handler(event, context):
     try:
@@ -63,6 +66,7 @@ def handler(event, context):
             result = {}
         else:
             result = get_price_history(params)
+
         return {
             "statusCode": 200,
             "body": result,
